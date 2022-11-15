3c3aecb49a158ccdb8d0541da4752809b08e9674
HamzaHammouti <hamzahhammouti@gmail.com>
2022-11-15 09:32:29 +0100

    Small visual updates

--- details.php
+++ details.php
@@ -12,18 +12,19 @@ require_once "config.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Details</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="bg-details.css">
     <style>@import url('https://fonts.googleapis.com/css2?family=Varela+Round&display=swap');</style>
 </head>
 <body>
-    <div class="left-half">
-        <div class="main_pic"><img src="img/400x600.png"></div>
-    </div>
-    <div class="right-half">
-        <div class="main_info">
-            <div class="main_desc"><a>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, asperiores autem. Repellat fuga molestiae perspiciatis a, aliquid at totam hic molestias dolorum laborum, enim unde! Ratione tempora quod ea reprehenderit expedita debitis, nulla veritatis aliquid facere, quis voluptates. Vel magni rerum obcaecati id, accusantium porro totam ex minima repellat consequuntur impedit assumenda vitae, fugiat, iste fuga distinctio delectus autem architecto numquam sunt alias cum cupiditate explicabo. Commodi, iure eaque? Quia.</a></div>
-            <div class="main_totalplayers"><h3>Maximaal {aantal} spelers.</h3></div>
-            <div class="main_join"><button type="button">Join the game!</button></div>
+        <div class="left-half">
+            <div class="main_pic"><img src="img/400x600.png"></div>
         </div>
-    </div>
+        <div class="right-half">
+            <div class="main_info">
+                <div class="main_desc"><a>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, asperiores autem. Repellat fuga molestiae perspiciatis a, aliquid at totam hic molestias dolorum laborum, enim unde! Ratione tempora quod ea reprehenderit expedita debitis, nulla veritatis aliquid facere, quis voluptates. Vel magni rerum obcaecati id, accusantium porro totam ex minima repellat consequuntur impedit assumenda vitae, fugiat, iste fuga distinctio delectus autem architecto numquam sunt alias cum cupiditate explicabo. Commodi, iure eaque? Quia.</a></div>
+                <div class="main_totalplayers"><h3>Maximaal {aantal} spelers.</h3></div>
+                <div class="main_join"><button type="button">Join the game!</button></div>
+            </div>
+        </div>
 </body>
 </html>
