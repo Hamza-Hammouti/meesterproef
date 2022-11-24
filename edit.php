f95c504dd67be1b2162da3036320c0aa1080579f
HamzaHammouti <hamzahhammouti@gmail.com>
2022-11-24 09:27:41 +0100

    Basic edit page started & working

---
+++ edit.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once "config.php";
+
+$id = $_GET['id'];
+$query = mysqli_query($db,"SELECT * FROM game_data WHERE id='$id'");
+while($row=mysqli_fetch_array($query)){
+
+        $name2 = $row["name"];
+        $max_players2 = $row["max_players"];
+        $game_desc2 = $row["game_desc"];
+        ?>
+            <form action="submit.php" method="post">
+                <label for="name">Name:</label><br>
+                <input type="text" id="name" name="name" value="<?php echo $name2 ?>"><br>
+                <label for="max_players">Max players:</label><br>
+                <input type="text" id="max_players" name="max_players" value="<?php echo $max_players2 ?>"><br>
+                <label for="game_desc">Game description:</label><br>
+                <textarea id="game_desc" name="game_desc" rows="4" cols="50"><?php echo $game_desc2 ?></textarea><br><br>
+                <input type="submit" value="Submit">
+            </form>
+        <?php
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit</title>
+</head>
+<body>
+    
+</body>
+</html>
