b0fbd69e859363f1a8ca48d22b4f346325436afd
HamzaHammouti <hamzahhammouti@gmail.com>
2022-11-24 11:04:25 +0100

    Confirm page added & working

---
+++ confirm.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once "config.php";
+
+$id = $_GET['id'];
+
+echo "Weet je zeker dat je dit wilt verwijderen?";
+
+?>
+<br><hr><button><a href="delete.php?id=<?php echo $id?>">Ja</a></button><button><a href="admin.php">Nee</a></button>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confirm</title>
+</head>
+<body>
+    
+</body>
+</html>
