8a8820e5e093e6adee35228065ccb7a9f3063ba5
HamzaHammouti <hamzahhammouti@gmail.com>
2022-11-28 09:54:30 +0100

    Create page made & working

---
+++ create.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once "config.php";
+
+        ?>
+            <form action="submit2.php" method="post">
+                <label for="name">Name:</label><br>
+                <input type="text" id="name" name="name" value=""><br>
+                <label for="max_players">Max players:</label><br>
+                <input type="text" id="max_players" name="max_players" value=""><br>
+                <label for="game_desc">Game description:</label><br>
+                <textarea id="game_desc" name="game_desc" rows="4" cols="50"></textarea><br><br>
+                <input type="submit" value="Submit">
+            </form>
+        <?php
+	
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create</title>
+</head>
+<body>
+    
+</body>
+</html>
