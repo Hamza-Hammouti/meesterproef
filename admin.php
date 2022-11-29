33235e98d700d60e71a551d40ccd2b2e0e21906a
HamzaHammouti <hamzahhammouti@gmail.com>
2022-11-29 13:51:59 +0100

    Visual changes and useless code removed

--- admin.php
+++ admin.php
@@ -3,18 +3,19 @@
 require_once "config.php";
 
 $show = mysqli_query($db,"SELECT * FROM game_data");
-$count=1;
 
+?>
+<div class="container"><div class="adminGameRow"></div>
+<?php
 if (mysqli_num_rows($show) > 0) {
 	while($row = mysqli_fetch_array($show)){
-  		?><table><b><button><a href="/meesterproef/edit.php?id=<?php echo $row["id"]?>">Edit</a></button><button><a href="/meesterproef/confirm.php?id=<?php echo $row["id"]?>">Delete</a></button><?php echo " ". $row["id"]?></b><?php echo " - " .$row["name"]." - " ?> <b> <?php echo $row["max_players"];?></b><br></table>
+  		?><table><b><div class="adminGame"><a href="/meesterproef/edit.php?id=<?php echo $row["id"]?>" class="editButton">EDIT</a><a href="/meesterproef/confirm.php?id=<?php echo $row["id"]?>" class="editButton2">DELETE</a><?php echo " ". $row["id"]?></b><?php echo " - " .$row["name"]." - " ?> <b> <?php echo $row["max_players"];?></div></b><br></table>
         
         <?php
-        $count=$count+1;
 	}
 }
 ?>
-<br><button><a href="/meesterproef/create.php">Create</a></button>
+<br><a href="/meesterproef/create.php" class="editButton3">Create</a></div>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -22,6 +23,8 @@ if (mysqli_num_rows($show) > 0) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="style.css">
     <title>Adminpanel</title>
 </head>
 <body>
